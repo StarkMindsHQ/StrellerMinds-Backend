69bf75d0cbde7f471f4f2ab328698094367ae46e
OlufunbiIK <olufunbiibrahim@gmail.com>
2026-01-23 00:07:26 +0100

    feat:Build Email and Notification System #441

---
+++ src/notifications/entities/notification-log.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity()
+export class NotificationLog {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  notificationId: string;
+
+  @Column()
+  event: 'DELIVERED' | 'OPENED' | 'CLICKED' | 'FAILED';
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
