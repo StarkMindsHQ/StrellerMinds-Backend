57fd40e2b8c619c8da8314dae7159235f06056e8
Divine Ifediorah <d-ifediorah@leadway.com>
2026-02-20 10:13:08 +0100

    ci/cd

--- src/forum/security-analytics.service.ts
+++ src/forum/security-analytics.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { SecurityIncident } from '../entities/security-incident.entity';
+import { SecurityIncident } from './security-incident.entity';
 
 @Injectable()
 export class SecurityAnalyticsService {
