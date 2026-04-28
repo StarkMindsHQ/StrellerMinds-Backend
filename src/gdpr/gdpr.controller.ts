6e413bc1b078eb2ff951b6075b5c6d5a6cd37176
LaGodxy <83363896+LaGodxy@users.noreply.github.com>
2026-04-28 04:02:06 -0700

    Revert "Feature/response streaming 808"

--- src/gdpr/gdpr.controller.ts
+++ src/gdpr/gdpr.controller.ts
@@ -11,8 +11,6 @@ import {
 import { Response } from 'express';
 import { DataRetentionService } from './data-retention.service';
 import { GdprService } from './gdpr.service';
-import { StreamResponse } from '../common/decorators/stream-response.decorator';
-import { StreamUtil } from '../common/utils/stream.util';
 
 @Controller('gdpr')
 export class GdprController {
@@ -22,18 +20,12 @@ export class GdprController {
   ) {}
 
   @Get('export/:userId')
-  @StreamResponse({ contentType: 'application/json' })
   async exportData(@Param('userId') userId: string, @Res() res: Response) {
     const result = await this.gdprService.exportUserData(userId);
     if (!result) throw new NotFoundException(`User ${userId} not found`);
-    
-    // Set headers for file download
     res.setHeader('Content-Type', result.mimeType);
     res.setHeader('Content-Disposition', `attachment; filename="${result.filename}"`);
-    
-    // Stream the data instead of loading it all into memory
-    const stream = StreamUtil.stringToStream(result.data, 8192); // 8KB chunks
-    stream.pipe(res);
+    res.send(result.data);
   }
 
   @Delete('users/:userId')
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,6 @@
-import { Controller, Get, Param, Query, Header, Res } from '@nestjs/common';
-import { Response } from 'express';
+import { Controller, Get, Param, Query, Header } from '@nestjs/common';
 import { UserService } from './user.service';
 import { EntityNotFoundException } from '../shared/domain/exceptions/domain-exceptions';
-import { StreamResponse } from '../common/decorators/stream-response.decorator';
-import { StreamUtil } from '../common/utils/stream.util';
 
 import { ListUsersUseCase, ListUsersRequest } from './application/use-cases/list-users.use-case';
 import { GetUserUseCase, GetUserRequest } from './application/use-cases/get-user.use-case';
@@ -17,31 +14,14 @@ export class UserController {
 
   @Get()
   @Header('X-Next-Cursor', '')
-  @StreamResponse({ contentType: 'application/json' })
   async findAll(
     @Query('search') search?: string,
     @Query('cursor') cursor?: string,
     @Query('limit') limit?: number,
-    @Query('stream') stream?: boolean,
-    @Res() res?: Response,
   ) {
     const defaultLimit = 20;
     const requestedLimit = limit ? parseInt(limit.toString(), 10) : defaultLimit;
-    
-    const result = await this.listUsersUseCase.execute(new ListUsersRequest(search, cursor, requestedLimit));
-
-    // Use streaming if requested and response object is available
-    if (stream && res) {
-      res.setHeader('X-Next-Cursor', result.nextCursor || '');
-      res.setHeader('X-Has-More', result.hasMore.toString());
-      
-      const jsonStream = StreamUtil.jsonArrayToStream(result.users, 50);
-      jsonStream.pipe(res);
-      return;
-    }
-
-    // Default behavior for non-streaming requests
-    return result;
+    return this.listUsersUseCase.execute(new ListUsersRequest(search, cursor, requestedLimit));
   }
 
   @Get(':id')
